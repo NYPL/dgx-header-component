72fc1d8e7c5c60ec526aa74d42b34aed8d3a5ffd
Sean Redmond <github-smr@sneakemail.com>
2016-01-11 15:24:04 -0500

    Add subscription Center link (WEB-1769)

--- src/components/EmailSubscription/EmailSubscription.js
+++ src/components/EmailSubscription/EmailSubscription.js
@@ -140,7 +140,12 @@ class EmailSubscription extends React.Component {
       // Always show the privacy link except in the loading phase.
       return (
         <div className={this.props.className}>
-          {subscribeContent}
+              {subscribeContent}
+          <a href={this.props.subCenterUrl}
+	    className={`${this.props.className}-sc-link`}
+	    style={styles.scLink}>
+	    Subscription Center
+	  </a>
           <a href={this.props.policyUrl}
             className={`${this.props.className}-pp-link`}
             style={styles.privacyLink}>
@@ -241,7 +246,8 @@ EmailSubscription.defaultProps = {
   list_id: '1061',
   form_method: 'POST',
   placeholder: 'Your email address',
-  policyUrl: 'http://www.nypl.org/help/about-nypl/legal-notices/privacy-policy'
+  policyUrl: 'http://www.nypl.org/help/about-nypl/legal-notices/privacy-policy',
+  subCenterUrl: 'http://pages.email.nypl.org/page.aspx?QS=3935619f7de112ef7250fe02b84fb2f9ab74e4ea015814b7',
 };
 
 const styles = {
@@ -283,13 +289,24 @@ const styles = {
   },
   privacyLink: {
     textDecoration: 'underline',
-    fontSize: '12px',
+    fontSize: '11px',
     color: 'rgba(255, 255, 255, 0.5)',
     fontWeight: '200',
+    textTransform: 'uppercase',
     position: 'absolute',
     bottom: '26px',
     right: '30px'
-  }
+  },
+  scLink: {
+    textDecoration: 'underline',
+    fontSize: '11px',
+    color: 'white',
+    fontWeight: '200',
+    textTransform: 'uppercase',
+    position: 'absolute',
+    bottom: '46px',
+    right: '30px'
+  },    
 };
 
 export default Radium(EmailSubscription);

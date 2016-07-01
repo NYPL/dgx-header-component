2ed4b3bbab83a76a129d2e5014cb350d9b72b008
Rafael Hernandez <rafaelhernandez@nypl.org>
2016-07-01 15:10:07 -0400

    Added eslint to header, added initial navConfig.js file to be exported along with Header.

---
+++ src/navConfig.js
@@ -0,0 +1,563 @@
+const current = [
+  {
+    id: '29a4b824-e084-4771-aafc-52b3586c5e49',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/browse',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Browse',
+      },
+    },
+    sort: 0,
+    subnav: [{
+      id: '92cf9abd-5491-48f8-bed4-a4a5bb1381c3',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'http://browse.nypl.org/iii/encore/homepage',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Books/Music/DVDs',
+        },
+      },
+      sort: 0,
+    }, {
+      id: '0aa57529-2883-4351-bb0e-639d9cdc4846',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/browse/recommendations',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Recommendations',
+        },
+      },
+      sort: 1,
+    }, {
+      id: '60a1ed93-dbb4-4b91-b370-59781998c5aa',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/blog',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Blog',
+        },
+      },
+      sort: 2,
+    }, {
+      id: '85e9c298-56c9-42af-922e-e72958fd105d',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/browse/public-projects',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Public Projects',
+        },
+      },
+      sort: 3,
+    }, {
+      id: '07c0268b-aecb-48a9-b1c1-05a244cb5865',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'http://shop.nypl.org',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Shop',
+        },
+      },
+      sort: 4,
+    }],
+  },
+  {
+    id: '925d1fa9-1138-403c-9990-38861902b02d',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/learn',
+      },
+      es: {
+        type: 'text-single',
+        text: 'learn',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Learn',
+      },
+      es: {
+        type: 'text-single',
+        text: 'Aprender',
+      },
+    },
+    sort: 1,
+    subnav: [{
+      id: '27843a66-82c0-4328-95af-4991ad6b084c',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/learn/kids',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Kids',
+        },
+      },
+      sort: 0,
+    }, {
+      id: '4ea49869-ec2c-4cc1-aad4-10b810ec1605',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/learn/teens',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Teens',
+        },
+      },
+      sort: 1,
+    }, {
+      id: '29feb93d-a219-4654-baf3-06f67e132f18',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/learn/parents',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Parents',
+        },
+      },
+      sort: 2,
+    }, {
+      id: '50159451-54ea-40ea-bae8-7c72b1d8e368',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/learn/teachers',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Teachers',
+        },
+      },
+      sort: 3,
+    }, {
+      id: 'f0ea8fad-c3b7-4c40-b00e-a39b527fadd6',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/learn/adults',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Adults',
+        },
+      },
+      sort: 4,
+    }],
+  },
+  {
+    id: '851ed351-08f5-43f0-9011-9c317f85f0ca',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/attend',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Attend',
+      },
+    },
+    sort: 2,
+    subnav: [{
+      id: '5752a16e-c1e0-4e18-b0e9-b51a51b3f6ed',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/events/calendar',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Events',
+        },
+      },
+      sort: 0,
+    }, {
+      id: '779c73fa-2545-4996-9dff-f8f9b7ab066e',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/attend/exhibitions',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Exhibitions',
+        },
+      },
+      sort: 1,
+    }, {
+      id: 'c17183a6-6cb8-476d-86da-9dc126ee5c67',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/events/tours',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Tours',
+        },
+      },
+      sort: 2,
+    }],
+  },
+  {
+    id: '4bd4f525-3f5c-4c45-b7a7-0ee6bbd301e9',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/research',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Research',
+      },
+    },
+    sort: 3,
+    subnav: [{
+      id: '30b7dfa2-9249-4d1f-9f5a-9fb64291372c',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/research/collections',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Collections',
+        },
+      },
+      sort: 0,
+    }, {
+      id: '0465eb1a-f318-4a9d-a404-4518a2a02d5c',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/research/information',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Information',
+        },
+      },
+      sort: 1,
+    }, {
+      id: 'adbfd5db-0a42-4833-9332-d6f0010c562e',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/research/services',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Services',
+        },
+      },
+      sort: 2,
+    }],
+  },
+  {
+    id: 'df621833-4dd1-4223-83e5-6ad7f98ad26a',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/locations/map',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Find Us',
+      },
+    },
+    sort: 4,
+    subnav: [{
+      id: '3d84f682-eae2-405b-b83e-b9fe83e9173b',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/locations/list',
+        },
+        es: {
+          type: 'text-single',
+          text: '/locations/list',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'All Locations',
+        },
+        es: {
+          type: 'text-single',
+          text: 'Todas Las Ubicaciones',
+        },
+      },
+      sort: 0,
+    }, {
+      id: '24236d1b-4797-449e-86b0-3cee54491fc5',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/locations/map?libraries=research',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Research Libraries',
+        },
+      },
+      sort: 1,
+    }],
+  },
+  {
+    id: '1d9ea0ec-6ca3-4577-9dd1-e8de1f2a8bb1',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/give',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Give',
+      },
+    },
+    sort: 5,
+    subnav: [{
+      id: '42781b1a-4ead-4871-830d-abf2121853ab',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/support/donate',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Donate',
+        },
+      },
+      sort: 0,
+    }, {
+      id: 'aee33c2f-d3c3-484a-b037-a00ea0a3028a',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/help/about-nypl/volunteer-nypl',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Volunteer',
+        },
+      },
+      sort: 1,
+    }],
+  },
+  {
+    id: '13d95ad5-f117-4415-ba2c-5c0b9618984d',
+    type: 'header-item',
+    link: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: '/get-help',
+      },
+    },
+    name: {
+      type: 'text-group',
+      en: {
+        type: 'text-single',
+        text: 'Get Help',
+      },
+    },
+    sort: 6,
+    subnav: [{
+      id: '87d2e065-4c44-4c90-833d-cc87ddaf4b0b',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/get-help/how-to',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'How to ...',
+        },
+      },
+      sort: 0,
+    }, {
+      id: 'ef8ff633-1fea-4f16-a3f3-46d2621f0a22',
+      type: 'header-item',
+      link: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: '/ask-nypl',
+        },
+      },
+      name: {
+        type: 'text-group',
+        en: {
+          type: 'text-single',
+          text: 'Contact Us',
+        },
+      },
+      sort: 1,
+    }],
+  },
+];
+
+const upcoming = [
+  {},
+];
+
+export const navConfig = {
+  current,
+  upcoming,
+};

883ee7dd379ce23b4ba03354cd374b60f11317c3
Kang Peng <kangtingpeng@nypl.org>
2016-12-19 15:14:40 -0500

    Added mock response of auth API.

---
+++ test/authApiMockResponse.js
@@ -0,0 +1,288 @@
+{
+  "data": {
+    "decodedToken": {
+      "iss": "https://www.nypl.org",
+      "sub": "6367028",
+      "aud": "app_login",
+      "iat": 1482176470,
+      "exp": 1482180070,
+      "auth_time": 1482176470,
+      "scope": "openid offline_access cookie patron:read"
+    },
+    "patron": {
+      "id": "6367028",
+      "updatedDate": "2016-12-13T19:49:57+00:00",
+      "createdDate": "2015-09-03T14:16:20+00:00",
+      "deletedDate": null,
+      "deleted": false,
+      "suppressed": false,
+      "names": [
+        "SMITH, THERESA",
+      ],
+      "barCodes": [
+        "23333097542730",
+      ],
+      "expirationDate": "2018-09-03",
+      "homeLibraryCode": "ma",
+      "birthDate": "1972-05-13",
+      "emails": [
+        "robkelley@nypl.org",
+      ],
+      "fixedFields": [
+        {
+          "label": "PDATE",
+          "value": "2016-11-21T16:54:11Z",
+          "display": null,
+        },
+        {
+          "label": "Education Level",
+          "value": "-",
+          "display": null,
+        },
+        {
+          "label": "School Code",
+          "value": "0",
+          "display": null,
+        },
+        {
+          "label": "Highest Overdues",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Record Type",
+          "value": "p",
+          "display": null,
+        },
+        {
+          "label": "Home Library",
+          "value": "ma   ",
+          "display": null,
+        },
+        {
+          "label": "FIRM",
+          "value": "",
+          "display": null,
+        },
+        {
+          "label": "Current Item D",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Expiration Date",
+          "value": "2018-09-03T08:00:00Z",
+          "display": null,
+        },
+        {
+          "label": "Patron Type",
+          "value": "10",
+          "display": null,
+        },
+        {
+          "label": "Patron Agency",
+          "value": "51",
+          "display": null,
+        },
+        {
+          "label": "PIUSE",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Record Number",
+          "value": "6367028",
+          "display": null,
+        },
+        {
+          "label": "Total Checkouts",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "ILL Request",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Current Item B",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Claims Returned",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "E-Communications",
+          "value": "-",
+          "display": null,
+        },
+        {
+          "label": "Overdue Penalty",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Preferred Language",
+          "value": "eng",
+          "display": null,
+        },
+        {
+          "label": "Patron Message",
+          "value": "-",
+          "display": null,
+        },
+        {
+          "label": "Agency",
+          "value": "1",
+          "display": null,
+        },
+        {
+          "label": "Current Item C",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Total Programs Attended",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Waitlists on Record",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Total Registrations",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Money Owed",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Updated Date",
+          "value": "2016-12-13T19:49:57Z",
+          "display": null,
+        },
+        {
+          "label": "Current Item A",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Manual Block",
+          "value": "-",
+          "display": null,
+        },
+        {
+          "label": "Registrations on Record",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "No. of Revisions",
+          "value": "91",
+          "display": null,
+        },
+        {
+          "label": "Debit Balance",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Home Region",
+          "value": "0",
+          "display": null,
+        },
+        {
+          "label": "Total Renewals",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Notice Preference",
+          "value": "-",
+          "display": null,
+        },
+        {
+          "label": "Last Circ Activity",
+          "value": "2016-12-13T19:49:57Z",
+          "display": null,
+        },
+        {
+          "label": "Birth Date",
+          "value": "1972-05-13",
+          "display": null,
+        },
+        {
+          "label": "Current Checkouts",
+          "value": 0,
+          "display": null,
+        },
+        {
+          "label": "Created Date",
+          "value": "2015-09-03T14:16:20Z",
+          "display": null,
+        },
+      ],
+      "varFields": [
+        {
+          "fieldTag": "=",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "dkMaWkbhuEXG6",
+          "subFields": null,
+        },
+        {
+          "fieldTag": "b",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "23333097542730",
+          "subFields": null,
+        },
+        {
+          "fieldTag": "e",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "19839",
+          "subFields": null,
+        },
+        {
+          "fieldTag": "z",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "robkelley@nypl.org",
+          "subFields": null,
+        },
+        {
+          "fieldTag": "a",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "123 ANYSTREET AVE$ANYWHERE, NY 10308",
+          "subFields": null,
+        },
+        {
+          "fieldTag": "n",
+          "marcTag": null,
+          "ind1": null,
+          "ind2": null,
+          "content": "SMITH, THERESA",
+          "subFields": null,
+        },
+      ],
+    },
+  },
+  "count": 1,
+  "statusCode": 200,
+  "debugInfo": [],
+}

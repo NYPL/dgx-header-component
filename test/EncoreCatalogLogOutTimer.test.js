eeb6bf6541499a20a7ec16234377888436fb62c0
Paul Beaudoin <contact@paulbeaudoin.com>
2019-08-20 14:28:48 -0400

    Change test on cookie write to be more fuzzy

--- test/EncoreCatalogLogOutTimer.test.js
+++ test/EncoreCatalogLogOutTimer.test.js
@@ -11,6 +11,7 @@ import accountConfig from '../src/accountConfig';
 // Matcher to fuzzily match a Unix ms value to within a few seconds:
 let matchWithinACoupleSecondsOf = (expectedTime, threshold = 2000) => {
   return sinon.match(function (actualTime) {
+    console.log(`Fuzzy matching a timestamp. Actual: ${actualTime}, Expected: ${expectedTime}, Difference: ${Math.abs(expectedTime - actualTime)}`)
     return Math.abs(expectedTime - actualTime) <= threshold
   })
 }
@@ -245,7 +246,7 @@ describe('EncoreLogOutTimer', () => {
         ).to.equal(true);
 
         // Verify cookie created with a value that's extremely recent
-        expect(setCookieSpy.calledWith('VALID_DOMAIN_LAST_VISITED', matchWithinACoupleSecondsOf(Date.now()))).to.equal(true);
+        expect(setCookieSpy.calledWith('VALID_DOMAIN_LAST_VISITED', matchWithinACoupleSecondsOf(Date.now(), 3000))).to.equal(true);
       });
   });
 

25d2538dff112efa778f768ae70e305bcdbeb073
Daniel Appel <dan.s.appel@gmail.com>
2019-12-10 16:16:07 -0500

    Add dist

---
+++ dist/gaConfig.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var gaConfig = {
+  eventCategory: 'Search',
+  eventAction: 'QuerySent',
+  customDimensions: {
+    dimension1: 'HeaderSearch',
+    dimension2: 'Unknown',
+    dimension4: 'NotSet',
+    dimension5: 'NotSet'
+  }
+};
+
+exports.default = gaConfig;
+module.exports = exports['default'];
